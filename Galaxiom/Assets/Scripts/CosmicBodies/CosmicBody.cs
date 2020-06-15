2e5035d74ce04977ba386107b1447916fcf3d39e
Sebastian Alkstrand <sebastian.alkstrand@gmail.com>
2020-06-15 23:18:23 +0200

    following tutorial by Sebastian Lague

--- Galaxiom/Assets/Scripts/CosmicBodies/CosmicBody.cs
+++ Galaxiom/Assets/Scripts/CosmicBodies/CosmicBody.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class CosmicBody : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(2, 255)]
+    public int resolution = 10;
+    public bool autoUpdate = true;
+
+    public ShapeSettings shapeSettings;
+    public ColorSettings colorSettings;
+
+    [HideInInspector]
+    public bool shapeSettingsFoldout;
+    [HideInInspector]
+    public bool colorSettingsFoldout;
+
+    ShapeGenerator shapeGenerator;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    private void Init()
     {
-        
+        shapeGenerator = new ShapeGenerator(shapeSettings);
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");    
+                meshObj.transform.parent = transform;   
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();    
+                meshFilters[i].sharedMesh = new Mesh(); 
+            }
+            terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);    
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GeneratePlanet()
     {
-        
+        Init();
+        GenerateMesh();
+        GenerateColors();
+    }
+
+    public void OnShapeSettingsUpdated()
+    {
+        if (!autoUpdate) return;
+        Init();
+        GenerateMesh();
+    }
+    
+    public void OnColorSettingsUpdates()
+    {
+        if (!autoUpdate) return;
+        Init();
+        GenerateColors();
+    }
+
+    private void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+
+    private void GenerateColors()
+    {
+        foreach (MeshFilter m in meshFilters)
+        {
+            m.GetComponent<MeshRenderer>().sharedMaterial.color = colorSettings.color;
+        }
     }
 }
---
+++ Galaxiom/Assets/Scripts/Editor/PlanetEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CosmicBody))]
+public class PlanetEditor : Editor
+{
+    CosmicBody planet;
+    Editor shapeEditor;
+    Editor colorEditor;
+
+    public override void OnInspectorGUI()
+    {
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            base.OnInspectorGUI();
+            if (check.changed)
+            {
+                planet.GeneratePlanet();
+            }
+        }
+
+        if (GUILayout.Button("Generate Planet"))
+        {
+            planet.GeneratePlanet();
+        }
+
+        DrawSettingsEditor(planet.shapeSettings, planet.OnShapeSettingsUpdated, ref planet.shapeSettingsFoldout, ref shapeEditor);
+        DrawSettingsEditor(planet.colorSettings, planet.OnColorSettingsUpdates, ref planet.colorSettingsFoldout, ref colorEditor);
+    }
+
+    void DrawSettingsEditor(Object settings, System.Action onSettingsUpdated, ref bool foldout, ref Editor editor)
+    {
+        if (settings == null) return;
+        foldout = EditorGUILayout.InspectorTitlebar(foldout, settings);
+
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            if (foldout)
+            {
+                CreateCachedEditor(settings, null, ref editor);
+                editor.OnInspectorGUI();
+
+                if (check.changed)
+                {
+                    if (onSettingsUpdated != null)
+                        onSettingsUpdated();
+                }
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        planet = (CosmicBody)target;
+    }
+}
